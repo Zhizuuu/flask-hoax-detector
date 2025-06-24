6590fd09bc506f228c6b55e24685da8b89351b47
Zhiddan Aditya <135048801+Zhizuuu@users.noreply.github.com>
2025-06-25 06:29:10 +0700

    fix: add/update Procfile with correct name

--- app.py
+++ app.py
@@ -1105,7 +1105,7 @@ def get_accuracy_by_label():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Membuat tabel jika belum ada
-    app.run(debug=True, host='0.0.0.0', port=int(environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(environ.get('PORT', 8000)))
 
 
 
